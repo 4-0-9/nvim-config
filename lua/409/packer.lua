051f5fbb24aa70c5e2c7453c8d8e347d10310c74
409 <bloecker.s@gmail.com>
2023-12-03 01:32:15 +0000

    init

---
+++ lua/409/packer.lua
@@ -0,0 +1,31 @@
+vim.cmd.packadd('packer.nvim')
+
+return require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+
+	use 'nvim-lua/plenary.nvim'
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.4',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	use({
+		'rose-pine/neovim',
+		as = 'rose-pine',
+		config = function()
+			vim.cmd('colorscheme rose-pine')
+		end
+	})
+
+	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+
+	use{
+		'ThePrimeagen/harpoon',
+		branch = 'harpoon2',
+		requires = { { 'nvim-lua/plenary.nvim' } }
+	}
+
+	use('mbbill/undotree')
+	use('tpope/vim-fugitive')
+end)

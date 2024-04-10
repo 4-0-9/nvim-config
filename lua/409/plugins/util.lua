e22288fbf8ef43fe0b6f468438ed93cb069f98c5
409 <bloecker.s@gmail.com>
2024-04-10 13:09:09 +0200

    added mini.surround

--- lua/409/plugins/util.lua
+++ lua/409/plugins/util.lua
@@ -1,24 +1,31 @@
 return {
-    {
-        "nvim-lua/plenary.nvim",
-        lazy = true,
-    },
-    {
-        "christoomey/vim-tmux-navigator",
-        lazy = false,
-    },
-    {
-        "mbbill/undotree",
-        keys = {
-            {
-                "<leader>u",
-                ":UndotreeToggle<CR>",
-            }
-        }
-    },
-    "nvim-tree/nvim-web-devicons",
-    {
-        "RRethy/vim-illuminate",
-        event = "VeryLazy"
+	{
+		"nvim-lua/plenary.nvim",
+		lazy = true,
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+	},
+	{
+		"mbbill/undotree",
+		keys = {
+			{
+				"<leader>u",
+				":UndotreeToggle<CR>",
+			},
+		},
+	},
+	"nvim-tree/nvim-web-devicons",
+	{
+		"RRethy/vim-illuminate",
+		event = "VeryLazy",
+	},
+	{
+        "echasnovski/mini.surround",
+        version = "*",
+        config = function()
+            require("mini.surround").setup()
+        end
     },
 }

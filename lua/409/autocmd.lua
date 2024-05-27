f7f6b22b7740f304adb0f8d6f82a96041cbbea66
409 <bloecker.s@gmail.com>
2024-05-28 00:09:59 +0200

    feat: yanked region flash

--- lua/409/autocmd.lua
+++ lua/409/autocmd.lua
@@ -1,9 +1,13 @@
-vim.api.nvim_create_autocmd({ 'FileType' }, {
-    pattern = { '*.yaml' },
-    command = 'setlocal ts=2 sts=2 sw=2 expandtab indentkeys-=0# indentkeys-=<:>'
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "*.yaml" },
+	command = "setlocal ts=2 sts=2 sw=2 expandtab indentkeys-=0# indentkeys-=<:>",
 })
 
-vim.api.nvim_create_autocmd({ 'FileType' }, {
-    pattern = { 'netrw' },
-    command = 'nnoremap <buffer> <C-l> <Cmd> TmuxNavigateRight<CR>'
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "netrw" },
+	command = "nnoremap <buffer> <C-l> <Cmd> TmuxNavigateRight<CR>",
+})
+
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+	command = "lua vim.highlight.on_yank {higroup='Visual',timeout=250}",
 })

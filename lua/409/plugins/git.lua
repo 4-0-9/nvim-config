76f158c76bfef4f5a4c03e856b6c16ea0de9a893
409 <bloecker.s@gmail.com>
2024-06-02 08:19:40 +0200

    feat: added neogit

--- lua/409/plugins/git.lua
+++ lua/409/plugins/git.lua
@@ -1,6 +1,32 @@
 return {
 	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+
+			"nvim-telescope/telescope.nvim", -- optional
 		},
+        keys = {
+            {
+                "<leader>gs",
+                ":Neogit<CR>",
+                desc = "Neogit"
+            },
+        },
+        opts = {
+            signs = {
+                item = { ">", "v" },
+                section = { ">", "v" },
+            },
+            mappings = {
+                status = {
+                    ["="] = "Toggle",
+                    ["<tab>"] = false,
+                },
+            },
+        },
+		config = true,
 	},
 	{
 		"echasnovski/mini.diff",

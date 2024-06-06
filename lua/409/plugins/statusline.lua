27b4c73a04c70c5a89bfcaf3c6b696b78b8386f7
409 <bloecker.s@gmail.com>
2024-06-06 11:49:41 +0200

    feat: lualine rectangular shape

--- lua/409/plugins/statusline.lua
+++ lua/409/plugins/statusline.lua
@@ -52,8 +52,8 @@ return {
 			options = {
 				icons_enabled = true,
 				theme = lualine_theme(),
-				component_separators = { left = "", right = "" },
-				section_separators = { left = "", right = "" },
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
 				disabled_filetypes = {
 					statusline = { "packer" },
 					winbar = {},
@@ -71,7 +71,7 @@ return {
 				lualine_a = { "mode" },
 				lualine_b = { "branch", "diff", "diagnostics" },
 				lualine_c = { "filename" },
-				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_x = { "fileformat", "filetype" },
 				lualine_y = { "progress" },
 				lualine_z = { "location" },
 			},

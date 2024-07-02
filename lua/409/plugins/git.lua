1ea27773df81d2c4c9d7b84543984217a60b0a2b
409 <bloecker.s@gmail.com>
2024-07-02 03:46:44 +0200

    feat!: added gitsigns in favor of mini.diff

--- lua/409/plugins/git.lua
+++ lua/409/plugins/git.lua
@@ -1,22 +1,59 @@
 return {
 	{
 		"tpope/vim-fugitive",
-        keys = {
-            {
-                "<leader>gs",
-                ":tab Git<CR>",
-                desc = "Fugitive"
-            },
-        },
-        lazy = true,
+		keys = {
+			{
+				"<leader>gs",
+				":tab Git<CR>",
+				desc = "Fugitive",
+			},
+		},
+		lazy = true,
 	},
 	{
-		"echasnovski/mini.diff",
-		version = false,
+		"lewis6991/gitsigns.nvim",
+		lazy = false,
+		keys = {
+			{
+				"<leader>hp",
+				":Gitsigns preview_hunk<CR>",
+				desc = "Preview hunk",
+			},
+			{
+				"<leader>hs",
+				":Gitsigns stage_hunk<CR>",
+				desc = "Stage hunk",
+			},
+			{
+				"<leader>hu",
+				":Gitsigns undo_stage_hunk<CR>",
+				desc = "Undo stage hunk",
+			},
+			{
+				"<leader>gb",
+				":Gitsigns toggle_current_line_blame<CR>",
+				desc = "Toggle current line blame",
+			},
+			{
+				"[c",
+				":Gitsigns next_hunk<CR>",
+				desc = "Jump to next hunk",
+			},
+			{
+				"]c",
+				":Gitsigns prev_hunk<CR>",
+				desc = "Jump to previous hunk",
+			},
+		},
+		opts = {
+			current_line_blame_opts = {
+				delay = 1000,
+			},
+		},
 		config = true,
 	},
-    {
-        "sindrets/diffview.nvim",
-        config = true,
-    },
+	{
+		"sindrets/diffview.nvim",
+		config = true,
+	},
 }

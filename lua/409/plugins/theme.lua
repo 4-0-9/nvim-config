d2b665c748cb6fff3b6c1ad63c3fc5ecb96f6425
409 <bloecker.s@gmail.com>
2024-08-08 16:20:25 +0200

    feat(theme): adjust border colors

--- lua/409/plugins/theme.lua
+++ lua/409/plugins/theme.lua
@@ -6,6 +6,13 @@ return {
 		priority = 1000,
 		config = function()
 			vim.cmd.colorscheme("dracula")
+
+			vim.api.nvim_set_hl(0, "QuickFixLine", { link = "Visual" })
+			vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = "#44475a" })
+			vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = "#44475a" })
+			vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = "#44475a" })
+			vim.api.nvim_set_hl(0, "TelescopePromptCounter", { fg = "#44475a" })
+			vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#44475a" })
 		end,
 	},
 }

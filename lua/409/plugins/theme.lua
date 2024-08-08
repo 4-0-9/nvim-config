f6e553087c70f88992d390f5e3bc5c16bde8c758
409 <bloecker.s@gmail.com>
2024-08-08 18:23:09 +0200

    feat(theme): tab highlights

--- lua/409/plugins/theme.lua
+++ lua/409/plugins/theme.lua
@@ -14,6 +14,9 @@ return {
 			vim.api.nvim_set_hl(0, "TelescopePromptCounter", { fg = "#44475a" })
 			vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#44475a" })
 			vim.api.nvim_set_hl(0, "PmenuSel", { fg = "#282a36", bg = "#50fa7b", bold = true })
+
+			vim.api.nvim_set_hl(0, "TabLine", { fg = "#f8f8f2", bg = "#44475a" })
+			vim.api.nvim_set_hl(0, "TabLineSel", { fg = "#282a36", bg = "#50fa7b", bold = true })
 		end,
 	},
 }
